cc0902635dc72c7c3ba4aacf19d284435f506a22
junki <hongjk7777@naver.com>
2021-11-19 03:55:31 +0900

    too late time cause function getResultOfColumn() that without dynamic programming

---
+++ src/BaekJoon_15684.java
@@ -0,0 +1,116 @@
+import java.util.Scanner;
+
+public class BaekJoon_15684 {
+
+    private static int min = 987654321;
+    private static int columnAmount, rowLineAmount, height;
+    private static int[][] rowLine = new int[30][9];
+
+    public static void main(String[] args) {
+        getInput();
+        rigLadderGame(3, rowLine);
+        if (min > 3) {
+            min = -1;
+        }
+        System.out.println(min);
+    }
+
+    private static void getInput() {
+        Scanner scanner = new Scanner(System.in);
+        columnAmount = scanner.nextInt();
+        rowLineAmount = scanner.nextInt();
+        height = scanner.nextInt();
+        for (int i = 0; i < rowLineAmount; i++) {
+            int lineHeight = scanner.nextInt() - 1;
+            int lineNum = scanner.nextInt() - 1;
+            rowLine[lineHeight][lineNum] = 1;
+        }
+    }
+
+
+    private static void rigLadderGame(int leftLadder, int[][] rowLine) {
+        //1을 찾앗으면 2는 x 알골
+        if (min <= 3 - leftLadder) {
+            return;
+        }
+
+        if (rigSuccess(rowLine)) {
+            int usedLadder = 3 - leftLadder;
+            if (usedLadder < min) {
+                min = usedLadder;
+            }
+            return;
+        }
+
+        if (leftLadder == 0) {  //개선 필요
+            return;
+        }
+
+
+
+        //for statement
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < columnAmount - 1; j++) {
+                int[][] clone = new int[30][9];
+                copyArray(rowLine, clone);
+
+                if (isPlaceable(i, j, clone) && clone[i][j] != 1) {
+                    clone[i][j] = 1;
+                    rigLadderGame(leftLadder - 1, clone);
+                }
+            }
+        }
+
+
+    }
+
+
+    private static boolean rigSuccess(int[][] rowLine) {
+        boolean rig = true;
+        int[][] cache = new int[30][9];
+
+        for (int i = 0; i < columnAmount; i++) {
+            if (getResultOfColumn(i, rowLine, cache) != i) {
+                rig = false;
+                break;
+            }
+        }
+        return rig;
+    }
+
+    private static int getResultOfColumn(int columnNum, int[][] rowLine, int[][] cache) {
+        int curColumn = columnNum; //column can change every height
+
+        for (int i = 0; i < height; i++) {
+            int tempColumn = curColumn;
+            if (tempColumn < columnAmount -1 && rowLine[i][tempColumn] == 1 ) {
+                curColumn++;
+            }
+
+            if (tempColumn > 0 && rowLine[i][tempColumn - 1] == 1) {
+                curColumn--;
+            }
+        }
+
+        return curColumn;
+    }
+
+    private static void copyArray(int[][] src, int[][] dest) {
+        for (int i = 0; i < 30; i++) {
+            for (int j = 0; j < 9; j++) {
+                dest[i][j] = src[i][j];
+            }
+        }
+    }
+
+
+    private static boolean isPlaceable(int height, int column, int[][] ladder) {
+        boolean placeable = column + 1 >= columnAmount - 1 || ladder[height][column + 1] != 1;
+
+        if (column > 0 && ladder[height][column - 1] == 1) {
+            placeable = false;
+        }
+        return placeable;
+    }
+
+}
