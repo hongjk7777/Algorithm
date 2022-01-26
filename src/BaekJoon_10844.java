8385dbe3c1d5312b1a1132d4cca1a6c87b6bc0cb
hongjk7777 <hongjk7777@naver.com>
2022-01-26 10:49:09 +0900

    solve BaekJoon_2193, 10844, 11053, 15990 with dp

---
+++ src/BaekJoon_10844.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon_10844 {
+    private static int n;
+    public static final int MOD = 1000000000;
+
+    //dp[i][j]는 j로 시작하는 크기 i의 계단 수의 개수
+    private static long[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        getInput();
+        makeDP();
+        printAnswer();
+    }
+
+    private static void getInput() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer;
+
+        tokenizer = new StringTokenizer(reader.readLine());
+        n = Integer.parseInt(tokenizer.nextToken());
+        dp = new long[n + 1][10];
+    }
+
+    private static void makeDP() {
+        initializeDP();
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j > 0) {
+                    dp[i][j] += dp[i - 1][j - 1];
+                }
+                if (j < 9) {
+                    dp[i][j] += dp[i - 1][j + 1];
+                }
+                dp[i][j] %= MOD;
+            }
+        }
+    }
+
+    private static void initializeDP() {
+        for (int i = 0; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+    }
+
+    private static void printAnswer() {
+        long sum = 0;
+        for (int i = 1; i < 10; i++) {
+            sum += dp[n][i];
+        }
+        long answer = sum % MOD;
+
+        System.out.println(answer);
+    }
+}
---
+++ src/BaekJoon_15990.java
@@ -0,0 +1,87 @@
+/*
+아쉬웠던 점
+1. 일차원 배열만 만드는 것에 익숙해져 있다가 이차원배열을 통해 dp를 해야되는
+아이디어를 너무 늦게 떠올려 시간을 너무 많이 잡아먹음
+2. 이 문제에서는 MOD가 int범위를 아슬아슬하게 지키는 범위라 마지막에 3개를 더했을 경우
+오버플로우가 발생할 수 있는데 이를 생각하지 못함
+ㄴ 앞으로는 int범위를 아슬아슬하게 지키는 경우 long으로 편하게 해도 될 듯
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon_15990 {
+    private static final int MAX = 1000000;
+    private static final int MOD = 1000000009;
+    private static int t;
+    private static int[] input, dpStart1, dpStart2, dpStart3;
+
+    public static void main(String[] args) throws IOException {
+        getInput();
+        makeDPs();
+        printAnswers();
+    }
+
+    private static void getInput() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer;
+
+        tokenizer = new StringTokenizer(reader.readLine());
+        t = Integer.parseInt(tokenizer.nextToken());
+        input = new int[t];
+        dpStart1 = new int[MAX];
+        dpStart2 = new int[MAX];
+        dpStart3 = new int[MAX];
+
+        for (int i = 0; i < t; i++) {
+            tokenizer = new StringTokenizer(reader.readLine());
+            input[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+    }
+
+    private static void makeDPs() {
+        initializeDPs();
+        for (int i = 4; i < MAX; i++) {
+            dpStart1[i] = (dpStart2[i - 1] + dpStart3[i - 1]) % MOD;
+            dpStart2[i] = (dpStart1[i - 2] + dpStart3[i - 2]) % MOD;
+            dpStart3[i] = (dpStart1[i - 3] + dpStart2[i - 3]) % MOD;
+        }
+    }
+
+    private static void initializeDPs() {
+        initializeDP1();
+        initializeDP2();
+        initializeDP3();
+    }
+
+    private static void initializeDP1() {
+        dpStart1[1] = 1;
+        dpStart1[2] = 0;
+        dpStart1[3] = 1;
+    }
+
+    private static void initializeDP2() {
+        dpStart2[1] = 0;
+        dpStart2[2] = 1;
+        dpStart2[3] = 1;
+    }
+    
+    private static void initializeDP3() {
+        dpStart3[1] = 0;
+        dpStart3[2] = 0;
+        dpStart3[3] = 1;
+    }
+
+    private static void printAnswers() {
+        for (int testcaseNum = 0; testcaseNum < t; testcaseNum++) {
+            printAnswer(testcaseNum);
+        }
+    }
+
+    private static void printAnswer(int testcaseNum) {
+        int temp = input[testcaseNum];
+        int answer = ((dpStart1[temp] + dpStart2[temp]) % MOD + dpStart3[temp]) % MOD;
+        System.out.println(answer);
+    }
+}
